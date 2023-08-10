3c55e9cb361d25411afe0966f30bd85f3a34c6db
Yusuf Yavuzyigit <66898768+Draconis-25@users.noreply.github.com>
2023-08-10 19:46:46 +0300

    Update build-appimage.sh

--- .github/scripts/build-appimage.sh
+++ .github/scripts/build-appimage.sh
@@ -6,6 +6,7 @@
    -DCMAKE_BUILD_TYPE="Debug" \ 
    -DENABLE_OPENMP="ON" \ 
    -DENABLE_SANITIZERS="ON" \ 
+   -DCMAKE_INSTALL_PREFIX=/usr \
    .. 
- make -j$(nproc) VERBOSE=1 
+ make -j$(nproc) VERBOSE=1 DESTDIR=AppDir
  make test_solvespace
